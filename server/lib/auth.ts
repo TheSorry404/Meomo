bd223b7d3382b14755f16d335fefad29ed8a6449
NicholasLD <878639947@qq.com>
2025-09-08 16:28:05 +0800

    fix: change the type of jwt payload

--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -6,7 +6,7 @@ const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "24h";
 const JWT_REFRESH_EXPIRES_IN = process.env.JWT_REFRESH_EXPIRES_IN || "7d";
 
 export interface JwtPayload {
-	userId: string;
+	userId: number;
 	email: string;
 	username: string;
 }
